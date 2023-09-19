a041fe8c515cd66c680afe5a3f1c6eadf60adf1a
Mehakb77 <b7mehak@gmail.com>
2023-09-19 23:43:58 +0530

    added validation back on notes

--- server/server.js
+++ server/server.js
@@ -17,17 +17,17 @@ const app = express();
 app.use(express.json());
 app.use(helmet());
 
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, "./assets");
-//   },
-//   filename: function (req, file, cb) {
-//     console.log("inside this fileName is", file);
-//     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-//     cb(null, uniqueSuffix + "-" + file.originalname);
-//   },
-// });
-// const upload = multer({ storage: storage });
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./assets");
+  },
+  filename: function (req, file, cb) {
+    console.log("inside this fileName is", file);
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, uniqueSuffix + "-" + file.originalname);
+  },
+});
+const upload = multer({ storage: storage });
 
 // app.post("/add-image", upload.single("image"), (req, res) => {
 //   return res
@@ -35,25 +35,20 @@ app.use(helmet());
 //     .json({ message: "file uploaded successfully" });
 // });
 
-// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-// app.use("/auth", authRouter);
-// app.use("/notes", notes);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/auth", authRouter);
+app.use("/notes", notes);
 
-// mongoose
-//   .connect(db_url, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => {
-//     server = app.listen(port, () => {
-//       console.log("start server at port", port);
-//       app.emit("appStarted");
-//     });
-//   });
-
-// addOne("Learn File system", "Clear filesystem task successfully");
-// findOne(1);
-
-startWritingStream();
+mongoose
+  .connect(db_url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    server = app.listen(port, () => {
+      console.log("start server at port", port);
+      app.emit("appStarted");
+    });
+  });
 
 module.exports = { app };
